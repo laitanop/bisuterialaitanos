e6acbe991656e3a82bdb3083e96197bfe8ccbfe5
laitanop <laitanop@yahoo.com>
2016-09-29 13:38:50 -0600

    mejorando

---
+++ js/scriptinicio.js
@@ -0,0 +1,32 @@
+
+
+var inicio = [
+{img: "i1.jpg" , nombre: "Coleccion Maya", descripcion: "Pulseras Macrame de colores con dije Estela Maya" },
+{img: "i2.jpg" , nombre: "Coleccion Maya", descripcion: "Pulseras Macrame de colores con dije Estela Maya" },
+{img: "i3.jpg" , nombre: "Coleccion Maya", descripcion: "Pulseras Macrame de colores con dije Estela Maya" },
+{img: "i4.jpg" , nombre: "Coleccion Maya", descripcion: "Pulseras Macrame de colores con dije Estela Maya" },
+{img: "i5.jpg" , nombre: "Coleccion Maya", descripcion: "Pulseras Macrame de colores con dije Estela Maya" },
+{img: "i6.jpg" , nombre: "Coleccion Maya", descripcion: "Pulseras Macrame de colores con dije Estela Maya" },
+
+];
+
+$(document).ready(function(){
+  for (i=0; i<inicio.length; i++){
+    $('#inicio').append(
+      
+
+
+      '<div class="col-sm-4 col-lg-4 col-md-4">'+
+                        '<div class="thumbnail">'+
+                            '<img src="../img/inicio/'+inicio[i].img+' "alt="">'+
+                            '<div class="caption">'+
+                                '<h4 class="pull-right"></h4>'+
+                                '<h4><a href="pulseras.html">'+inicio[i].nombre+'</a>'+
+                                '</h4>'+
+                                '<p> <a target="_blank" href="pulseras.html">'+inicio[i].descripcion+'</a></p>'+
+                            '</div>'
+                            );
+  }
+});
+
+
